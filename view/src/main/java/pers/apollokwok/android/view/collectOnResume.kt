2ff3e42643aaa908359396d4d17e920e052d9867
WilliamKwokX <1425183716@qq.com>
2023-05-23 11:21:08 +0800

    add requirement in 'collectOnResume'

--- view/src/main/java/pers/apollokwok/android/view/collectOnResume.kt
+++ view/src/main/java/pers/apollokwok/android/view/collectOnResume.kt
@@ -13,6 +13,13 @@ import kotlinx.coroutines.launch
  */
 context(Fragment)
 public fun <T> Flow<T>.collectOnResume(collector: FlowCollector<T>){
+    require(
+        viewLifecycleOwnerLiveData.value != null
+        && viewLifecycleOwner.lifecycle.currentState == Lifecycle.State.INITIALIZED
+    ){
+        "Call this function in 'onCreateView' or 'onViewCreated'."
+    }
+
     viewLifecycleOwner.lifecycleScope.launch {
         repeatOnLifecycle(Lifecycle.State.RESUMED) {
             collect(collector)
