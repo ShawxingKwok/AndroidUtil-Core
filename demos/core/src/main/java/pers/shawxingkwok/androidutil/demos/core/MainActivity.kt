d23c6c12d9e4ad35dd0badab02878817e4ea34b2
WilliamKwokX <1425183716@qq.com>
2023-06-01 00:16:45 +0800

    +

--- demos/core/src/main/java/pers/shawxingkwok/androidutil/demos/core/MainActivity.kt
+++ demos/core/src/main/java/pers/shawxingkwok/androidutil/demos/core/MainActivity.kt
@@ -2,7 +2,6 @@ package pers.shawxingkwok.androidutil.demos.core
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
