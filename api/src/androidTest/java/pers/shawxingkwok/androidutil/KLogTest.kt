e212de38f5c524e25c75c1dfb7ed086b230fd5a2
WilliamKwokX <1425183716@qq.com>
2023-07-30 00:56:56 +0800

    +

--- api/src/androidTest/java/pers/shawxingkwok/androidutil/KLogTest.kt
+++ api/src/androidTest/java/pers/shawxingkwok/androidutil/KLogTest.kt
@@ -1,5 +1,8 @@
 package pers.shawxingkwok.androidutil
 
+import android.view.View
+import android.widget.Button
+import androidx.core.view.isVisible
 import org.junit.Test
 
 internal class KLogTest {
