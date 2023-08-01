72c8bb30f5c36eb566f414e4863df5786a69eebb
WilliamKwokX <1425183716@qq.com>
2023-08-01 11:30:10 +0800

    +

--- api/src/main/java/pers/shawxingkwok/androidutil/AppContext.kt
+++ api/src/main/java/pers/shawxingkwok/androidutil/AppContext.kt
@@ -4,15 +4,15 @@ import android.content.Context
 import androidx.startup.Initializer
 
 /**
- * It is the static applicationContext for you to get anywhere. In this way,
- * your `Database`, `DAO`, `SharedPreferences`, `DataStore` and some other tools
- * could be static, of which the profit is much more considerable than the
- * additional resource memory.
+ * See [docs](https://shawxingkwok.github.io/ITWorks/docs/android/util-core/#appcontext).
  */
 public lateinit var AppContext: Context
     private set
 
-internal class AppContextInitializer : Initializer<Unit> {
+/**
+ * See [docs](https://shawxingkwok.github.io/ITWorks/docs/android/util-core/#appcontext).
+ */
+public class AppContextInitializer : Initializer<Unit> {
     override fun create(context: Context) {
         AppContext = context
     }
